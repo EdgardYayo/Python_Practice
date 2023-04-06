eadc4dc69c248f49b7beadad59d823d2c286689c
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-04-06 14:40:15 -0700

    feat: types and variables python

---
+++ 00_types.py
@@ -0,0 +1,26 @@
+# Asi se escribe un comentario en python
+# Hola Mundo con Python 
+print("Hello World") 
+print('Hello World') 
+
+""" Este es un comentario en
+varias lineas asi es como se escribe """
+
+# Types of Numbers
+# Integers
+print(1 ,2 ,3)
+
+#Float
+print(1.23, 3.45, 5.89)
+
+#Complex 
+print(3j)
+
+
+# Como consultar el tipo de dato con type()
+print(type("Hello")) # type 'str'
+print(type(3)) # type 'int'
+print(type(4.5)) # type 'float'
+print(type(3j)) # type 'complex'
+print(type(True)) # type 'bool'
+print(type([ 1, 2, 3])) # type 'list'
---
+++ 01_variables.py
@@ -0,0 +1,28 @@
+# Variables
+# En python ahi que escribir las variables en snake case osea con "_"
+
+my_variable = "My String Variable"
+print(my_variable)
+
+my_number = 12
+print(my_number)
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# Funciones del Sistema
+# La funcion print() puede recibir varios parametros
+# el metodo len() devuelve el largo de una list o un str
+# el metodo str() convierte un numero a un string
+# el metodo int() convierte un string a un entero pero debe ser un string con caracteres numericos
+
+print(my_variable, 'tiene un largo de:', len(my_variable), 'caracteres')
+print(type(str(my_number)))
+
+my_numy = "2"
+number = int(my_numy)
+
+print(type(my_numy))
+print(my_numy)
+print(type(number))
+print(number)
