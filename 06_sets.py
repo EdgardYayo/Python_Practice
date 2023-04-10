7a16e784550087e8a5664d679b9c817f2b47f2c4
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-04-09 18:38:36 -0700

    feat: studying sets and their methods

--- 06_sets.py
+++ 06_sets.py
@@ -1,1 +1,31 @@
 # Sets ✔
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set)) # Inicialmente es un diccionario
+
+my_other_set = {"Edgard", "Pazos", 28, 1.77}
+print(type(my_other_set))
+print(my_other_set)
+
+my_new_set = set("Edgard")
+print(my_new_set)
+print(len(my_new_set)) # len() para saber cuentos elementos tiene el set
+
+# Con add() podemos añadir un nuevo elemento al set
+my_new_set.add("hello")
+print(my_new_set) # Un set no es una estructura ordenada
+
+# En un set cada elemento solo puede estar una vez (no admite repetidos)
+my_new_set.add("hello")
+print(my_new_set)
+
+# Se puede comprobar si un elemento existe en un set con el operador "in"
+print("hello" in my_new_set)
+print("he" in my_new_set)
+
+# Se puede eliminar un elemento del set con el metodo remove()
+my_new_set.remove("hello")
+print(my_new_set)
