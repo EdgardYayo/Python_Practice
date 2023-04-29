a15ea0f193505612bdb1f3823f3f83e4524fa5c8
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-04-28 17:34:34 -0700

    feat: ending dicts lesson and starting if lessons

--- 07_dicts.py
+++ 07_dicts.py
@@ -13,7 +13,36 @@ my_other_dict = {
     "languages": ["JavaScript", "Python"]
 }
 
+# Se puede acceder a los valores de las claves de los diccionarios
+# asi nombredeldiccionario[clave] y esto nos daria el valor
 print(my_other_dict)
 print(my_other_dict["id"])
 print(my_other_dict["name"])
 print(my_other_dict["languages"][0])
+
+print(len(my_other_dict))
+
+
+# Asi como tambien cambiar los valores de las claves
+my_other_dict["lastname"] = "Inda"
+print(my_other_dict)
+
+# Para borrar una clave-valor de un diccionario con la palabra clave "del"
+del my_other_dict["id"]
+print(my_other_dict)
+
+# Para comprobar si una clave existe en el diccionario con la palabra clave "in"
+print("Edgard" in my_other_dict) # False
+print("name" in my_other_dict) # True
+
+# Para obtener todos los elementos clave-valor de un diccionario con el metodo items()
+print(my_other_dict.items())
+
+
+# Para obtener las claves de un diccionarios con el metodo keys() y los valores con el metodo values()
+print(my_other_dict.keys()) 
+print(my_other_dict.values()) 
+
+# Para crear un diccionario con claves con valores none con el metodo "fromkeys()"
+my_new_dict = dict.fromkeys(("Nombre", "Apellido"))
+print(my_new_dict)
---
+++ 08_conditionals.py
@@ -0,0 +1,24 @@
+# Conditionals ✔
+
+my_condition = True
+
+if my_condition:
+    print("Verdadero")
+else:
+    print("Falso")
+
+my_condition = 5 * 2
+
+if my_condition == 11:
+    print("se ejecuta la condicion de mi segundo if")
+
+
+if my_condition > 10 and my_condition < 20:
+    print("mayor que 10 y menor que 20")
+else:
+    print("es menor o igual que 10")
+
+
+
+
+print("Otra cosa")
