1adb0789fb12333aef3e88181857b6165084b8ad
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-05-15 12:09:26 -0700

    feat: finishing loops lesson and start functions

--- 09_loops.py
+++ 09_loops.py
@@ -1,6 +1,6 @@
 # Loops ✔
 
-# While
+# Bucles While ######################
 
 
 one_condition = True
@@ -13,6 +13,7 @@ while one_condition:
     if id == 7: # Añadiendo esta linea el bucle se detendria cuando el id valga 7
         break
 
+# La palabra reservada "break" sirve para detener la ejecucion de los ciclos o bucles
 
 other_condition = 20
 
@@ -20,8 +21,49 @@ while other_condition < 30:
     print("Sigo siendo menor que 20")
     other_condition += 1
 
+# Bucles For ##################
 
-# ME QUEDE EN EL TIEMPO 7:00
+my_list = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
 
+for element in my_list:
+    print(element * 2)
+
+# Imprimiría 1, 4, 6, 8, 10, 12, 14, 16
+
+my_tuple = (32, "Hola", True)
+
+my_set = { "Hola", 2, 3, False }
+
+my_dict = { "Name": "Edgard", "Edad": 5 }
+
+for element in my_tuple:
+    print(element)
+
+for element in my_set:
+    print(element)
+
+for element in my_dict:
+    print(element)
+
+# Con el bucle For podemos recorrer e imprimir todos los elementos de listas, tuplas, sets y diccionarios, ademas podemos hacer operaciones con este elementos durantes el bucle
+
+new_list = [ 3, 4, 5, 6, 8 ]
+
+for ele in new_list:
+    if ele == 5:
+        print(ele * 2)
+    elif ele == 6: 
+        break
+    else: 
+        print(ele)
+
+
+# Existe otra palabra reservada "continue" que tiene el propósito de saltar a la siguiente iteración del bucle sin ejecutar el código restante dentro del bloque de bucle actual.
+
+for ele in new_list:
+    if ele == 4:
+        continue
+    else:
+        print(ele)
 
 
---
+++ 10_functions.py
@@ -0,0 +1,22 @@
+# Functions ✔
+
+# Para crear una función se utiliza para iniciar la palabra reservada "def"
+
+def hola():
+    print("Hola") 
+
+# Despues se invoca la función para que se inicie su ejecucion
+
+hola()
+
+# Una función tambien puede recibir parametros o argumentos para trabajar con ellos
+
+def is_a_sum(a, b):
+    my_sum = a + b
+    return my_sum
+
+# Despues la invocamos o llamamos pasandole los parametros o argumentos que necesita para trabajar
+
+is_a_sum(4, 10)
+
+# ME QUEDE EN EL TIEMPO 7:40
