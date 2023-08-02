0bec931ade37770f695991dbf763d028d67a2db3
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-08-02 09:53:14 -0700

    Adding debug exercises

---
+++ debug_exercise.py
@@ -0,0 +1,49 @@
+############DEBUGGING#####################
+
+# # Describe Problem
+# def my_function():
+#   The problem is the for loop is looping only from 1 to 19 because 20 is non inclusive
+#   for i in range(1, 20):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# # Reproduce the Bug
+# from random import randint
+# dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+# The problem is when randint function choose randomly 6 the list is out of range
+# dice_num = randint(1, 6)
+# print(dice_imgs[dice_num])
+
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#   print("You are a millenial.")
+# elif year >= 1994:
+#   print("You are a Gen Z.")
+
+# # Fix the Errors
+# The print inside the the if statement needs indentation
+# The input is a str need to be integer to do comparison
+# And the last print need to be an f string in order to print the value of the variable age
+# age = input("How old are you?")
+# if age > 18:
+# print("You can drive at age {age}.")
+
+# #Print is Your Friend
+# pages = 0
+# word_per_page = 0
+# pages = int(input("Number of pages: "))
+# word_per_page == int(input("Number of words per page: "))
+# total_words = pages * word_per_page
+# print(total_words)
+
+# #Use a Debugger
+# def mutate(a_list):
+#   b_list = []
+#   for item in a_list:
+#     new_item = item * 2
+#   b_list.append(new_item)
+#   print(b_list)
+
+# mutate([1,2,3,5,8,13])
