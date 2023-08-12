e385620f4e59a9621063f470674b196fa102be0c
EdgardYayo <allan.pazos@uabc.edu.mx>
2023-08-12 15:35:47 -0700

    Just Created a OOP Quiz Game.

--- OOP/object_oriented_programming.py
+++ OOP/object_oriented_programming.py
@@ -1,8 +1,34 @@
-from prettytable import PrettyTable
+# THESE ARE VERY BASIC EXAMPLES OF USINGS OBJECTS AND CREATE THEM IN PYTHON.
+# from prettytable import PrettyTable
+#
+# table = PrettyTable()
+# table.add_column("Pokémon Name", ["Pikachu", "Bulbasaur"])
+# table.add_column("Type", ["Electric", "Grass"])
+#
+# table.align = "l"
+# print(table)
 
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Pikachu", "Bulbasaur"])
-table.add_column("Type", ["Electric", "Grass"])
+class User:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.followers = 0
+        self.following = 0
 
-table.align = "l"
-print(table)
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+
+
+user_1 = User("Edgard", 28, "Male")
+user_2 = User("Angela", 32, "Female")
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
+print(user_2.followers)
+
